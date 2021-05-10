972e926eb5db21679e6421f2ec7b093fae883f99
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-05-09 22:48:07 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ import NavBar from './components/NavBar';
 import AboutMe from './components/AboutMe';
 //import { Route, Switch, Redirect } from 'react-router-dom';
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Redirect } from 'react-router-dom';
 
 const App = () => {
 
     return (
         <div>
-            <Router>
+            <HashRouter>
                 <NavBar />
-                <Switch>
-                    <Route exact path="/Home" component={Home} />
-                    <Route exact path="/">
-                        <Redirect to="/Home" />
-                    </Route>
-                    <Route exact path="/AboutMe" component={AboutMe} />
-                    <Route exact path="/Projects" component={Projects} />
-                    <Route exact path="/ContactMe" component={ContactMe} />
-                </Switch>
-            </Router>
+                <Route exact path="/" component={Home} />
+                <Route path="/Home" component={Home} />
+                <Route path="/AboutMe" component={AboutMe} />
+                <Route path="/Projects" component={Projects} />
+                <Route path="/ContactMe" component={ContactMe} />
+            </HashRouter>
+
         </div>
 
     )
 }
 
 export default App;
+
+// <Router>
+//                 <NavBar />
+//                 <HashRouter>
+//                     <Route exact path="/Home" component={Home} />
+//                     <Route exact path="/">
+//                         <Redirect to="/Home" />
+//                     </Route>
+//                     <Route exact path="/AboutMe" component={AboutMe} />
+//                     <Route exact path="/Projects" component={Projects} />
+//                     <Route exact path="/ContactMe" component={ContactMe} />
+//                 </HashRouter>
+//             </Router>
+//         </div>
