16452a54584fb6e3b454e213b0d2a31af3fc1a10
Martje55555 <martje0629@gmail.com>
2022-05-04 01:24:14 -0500

    added basename public url

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import ContactMe from './components/ContactMe';
 import NavBar from './components/NavBar';
 import AboutMe from './components/AboutMe';
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 const App = () => {
 
     return (
         <div>
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <NavBar />
                 <Routes>
                     <Route exact path="/" element={<Home />} />
