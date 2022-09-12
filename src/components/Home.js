baa88cd90eed9535ce937c028528ab0b45d573ed
Martje55555 <martje0629@gmail.com>
2022-09-11 23:45:54 -0500

    update experience

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,14 +33,10 @@ const Home = () => {
                     I attended High School at Mission High School and graduated in 2018.
                     I graduated as a Mission Merit, and in the top 10% of my class. I am
                     currently a Senior attending the University of Texas Rio Grande Valley,
-                    pursuing a degree in Computer Engineering. When I am not studying I enjoy
-                    playing video games, watching sports and training Brazilian Jiu-Jitsu. I previously worked
-                    as a Software Engineer Intern at <a href="https://www.lob.com/" target="_blank">Lob </a>
-                    where we build APIs to Increase Connectivity Between the Offline and Online Worlds.
-                    I currently work as an Undergraduate Research Assistant in the area of artificical intelligence,
-                    but more specifically in autonomous transportation, a project funded by the NSF and as a member of the 
-                    <a target={"_blank"} href={"https://fatemehnazari.weebly.com/"}> H-SMART Lab</a>. I am also a Software 
-                    Engineer Intern at P&G for the Summer of 2022.
+                    pursuing a degree in Computer Engineering. I am currently a Data Science
+                    Intern at USDA for the Fall of 2022. My interests include Machine Learning, 
+                    Back-End Development, and Application Security. When I am not studying or working
+                    my hobbies include Brazilian Jiu-Jitsu (BJJ), Basketball, and Video Games.
                 </b></p></center>
 
             </div>
